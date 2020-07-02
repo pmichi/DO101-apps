e91a09e068840e11c6a1b980559e50386ccf7226
pmichi <p.michieletto@gmail.com>
2020-07-02 16:38:59 +0200

    Update app.js

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -2,7 +2,7 @@ var express = require('express');
 app = express();
 
 app.get('/mars', function (req, res) {
-  res.send('Hello Mars!\n');
+  res.send('Hello, voyage to Mars!\n');
 });
 
 app.listen(8080, function () {
